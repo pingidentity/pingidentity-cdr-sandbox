0d76f8196f0c898b6a3c9867d3391c2c8377c646
Tam Tran <ttran@pingidentity.com>
2020-08-01 00:34:15 +1000

    cdr-165: Initial k8 conversion

--- server_profiles/pingaccess/hooks/80-post-start.sh
+++ server_profiles/pingaccess/hooks/80-post-start.sh
@@ -5,24 +5,30 @@
 #- This script is used to import any configurations that are
 #- needed after PingAccess starts
 
-# shellcheck source=../../../../pingcommon/opt/staging/hooks/pingcommon.lib.sh
+# shellcheck source=pingcommon.lib.sh
 . "${HOOKS_DIR}/pingcommon.lib.sh"
 
-if test -z "${OPERATIONAL_MODE}" || test "${OPERATIONAL_MODE}" = "CLUSTERED_CONSOLE" || test "${OPERATIONAL_MODE}" = "STANDALONE" 
-then
-    echo "INFO: waiting for PingAccess to start before importing configuration"
-    wait-for localhost:${PA_ADMIN_PORT} -t 200 -- echo PingAccess is up
-    "${HOOKS_DIR}/81-after-start-process.sh"
-    test ${?} -ne 0 && kill 1
+if test -z "${OPERATIONAL_MODE}" || test "${OPERATIONAL_MODE}" = "STANDALONE"  ; then
+  echo "INFO: waiting for PingAccess to start before importing configuration"
+    while true; do
+    curl -ss --silent -o /dev/null -k https://localhost:9000/pa/heartbeat.ping
+    if ! test $? -eq 0 ; then
+        echo "Starting PA Admin: Server not started, waiting.."
+        sleep 5
+    else
+        echo "PA started, begin configuration"
+	break
+    fi
+    done
+        sleep 30
+  	${HOOKS_DIR}/81-after-start-process.sh
 
-    ${HOOKS_DIR}/90-install-ca.sh
-    ${HOOKS_DIR}/91-install-keypair.sh
+        sleep ${PA_POST_CONFIGURATION_DELAY}
+        ${HOOKS_DIR}/90-install-ca.sh
+        ${HOOKS_DIR}/91-install-keypair.sh
 fi
 
-if test "${OPERATIONAL_MODE}" = "CLUSTERED_CONSOLE"
-then
-    echo "Bringing eth0 back up..."
-    ip link set eth0 up
+if test "${OPERATIONAL_MODE}" = "CLUSTERED_CONSOLE"  ; then
+  echo "Bringing eth0 back up..."
+  ip link set eth0 up
 fi
-
-
