d159dbbbf44dc851d81c83dbdf9b656dc2868760
Tam Tran <ttran@pingidentity.com>
2020-09-07 15:11:25 +1000

    cdr-11: convert PF config to bulk load api

--- scripts/pf-bulkconfigtools/export-config.sh
+++ scripts/pf-bulkconfigtools/export-config.sh
@@ -1,8 +1,12 @@
+function prop {
+    grep "${1}" ../../docker-compose/pf.env|cut -d'=' -f2
+}
+
 echo "Creating output folder..."
 mkdir out
 
 echo "Downloading config from https://localhost:9999..."
-curl -X GET --basic -u Administrator:2FederateM0re --header 'Content-Type: application/json' --header 'X-XSRF-Header: PingFederate' https://localhost:9999/pf-admin-api/v1/bulk/export --insecure > out/pf-export.json
+curl -X GET --basic -u Administrator:$(prop 'administrativeAccounts_items_Administrator_password') --header 'Content-Type: application/json' --header 'X-XSRF-Header: PingFederate' https://localhost:9999/pf-admin-api/v1/bulk/export --insecure > out/pf-export.json
 
 echo "Creating/modifying docker-compose/pf.env and server-profiles/pingfederate/instance/import-bulkconfig.json.subst..."
 java -jar pf-bulkexport-tools-0.0.1-SNAPSHOT-jar-with-dependencies.jar config.json ./out/pf-export.json ../../docker-compose/pf.env ../../server_profiles/pingfederate/instance/import-bulkconfig.json.subst > out/export-convert.log
