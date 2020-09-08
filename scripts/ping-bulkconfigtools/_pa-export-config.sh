b30c529ecf35d9b69e7f7a6596f0af317c95e6db
Tam Tran <ttran@pingidentity.com>
2020-09-08 10:47:48 +1000

    cdr-11: convert PA config to bulk load api

--- scripts/ping-bulkconfigtools/_pa-export-config.sh
+++ scripts/ping-bulkconfigtools/_pa-export-config.sh
@@ -9,7 +9,7 @@ echo "Downloading config from https://localhost:9000..."
 curl -X GET --basic -u Administrator:$(prop 'PING_IDENTITY_PASSWORD') --header 'Content-Type: application/json' --header 'X-XSRF-Header: PingAccess' https://localhost:9000/pa-admin-api/v3/config/export --insecure > out/pa-export.json
 
 echo "Creating/modifying docker-compose/pa.env and server_profiles/pingaccess/instance/data/start-up-deployer/data.json.subst..."
-java -jar ping-bulkexport-tools-0.0.1-SNAPSHOT-jar-with-dependencies.jar pa-config.json ./out/pa-export.json ../../docker-compose/pa.env ../../server_profiles/pingaccess/instance/data/start-up-deployer/data.json.subst > out/pa-export-convert.log
+java -jar ping-bulkexport-tools-project/target/ping-bulkexport-tools-0.0.1-SNAPSHOT-jar-with-dependencies.jar pa-config.json ./out/pa-export.json ../../docker-compose/pa.env ../../server_profiles/pingaccess/instance/data/start-up-deployer/data.json.subst > out/pa-export-convert.log
 
 #echo "Copying pf.env to k8 server profile..."
 #cp ../../docker-compose/pf.env ../../k8/02-completeinstall
