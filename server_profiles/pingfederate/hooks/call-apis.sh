abfa8bf27243ffdcb8d49c9bebef58fc65635bc8
Tam Tran <ttran@pingidentity.com>
2020-09-11 10:40:46 +1000

    Bootstrapping generic API admin calls

--- server_profiles/pingfederate/hooks/call-apis.sh
+++ server_profiles/pingfederate/hooks/call-apis.sh
@@ -20,13 +20,14 @@ API_DROPIN_FOLDER=/opt/out/instance/server/default/drop-in-config
 apiFolder=
 
 if test -d "${API_DROPIN_FOLDER}" ; then
-    for apiFolderName in $( find "${API_DROPIN_FOLDER}/*" -type d 2>/dev/null | sort | uniq ) ; do
+    for apiFolderName in $( find "${API_DROPIN_FOLDER}/" -type d 2>/dev/null | sort | uniq ) ; do
         apiFolder=$(basename ${apiFolderName})
         echo "API Folder: $apiFolder"
         requestUrl=$(apiprops 'request-url')
         curlParams=$(apiprops 'request-curl-commands')
+        curlCommand="curl $curlParams -d @${apiFolderName}/requestBody.json $requestUrl --insecure --write-out '%{http_code}' --silent --output /dev/null"
         echo "Calling API: $requestUrl with $curlParams"
-        httpStatus=$(curl $curlParams -d @${apiFolderName}/requestBody.json $requestUrl --insecure --write-out '%{http_code}' --silent --output /dev/null)
+        httpStatus=$(eval $curlCommand)
 
         echo "HTTP Status Code: ${httpStatus}"
     done
