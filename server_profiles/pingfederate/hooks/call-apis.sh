9959814dbcb573eb68d5e0da6b0e920a65d026a8
Tam Tran <ttran@pingidentity.com>
2020-09-11 10:34:02 +1000

    Bootstrapping generic API admin calls

--- server_profiles/pingfederate/hooks/call-apis.sh
+++ server_profiles/pingfederate/hooks/call-apis.sh
@@ -3,15 +3,15 @@
 # Ping Identity DevOps - Docker Build Hooks
 #
 
-function prop {
-    echo "Reading properties file: $API_DROPIN_FOLDER/$apiFolder/request.properties"
-    grep "${1}" $API_DROPIN_FOLDER/$apiFolder/request.properties|cut -d'=' -f2
-}
-
 ${VERBOSE} && set -x
 
 # shellcheck source=../../../../pingcommon/opt/staging/hooks/pingcommon.lib.sh
 . "${HOOKS_DIR}/pingcommon.lib.sh"
+
+function apiprops {
+    echo "Reading properties file: $API_DROPIN_FOLDER/$apiFolder/request.properties"
+    grep "${1}" $API_DROPIN_FOLDER/$apiFolder/request.properties|cut -d'=' -f2
+}
 
 # ideally replace hardcoded drop-in folder to staging directory
 # currently not in staging because we require subst process
@@ -20,13 +20,14 @@ API_DROPIN_FOLDER=/opt/out/instance/server/default/drop-in-config
 apiFolder=
 
 if test -d "${API_DROPIN_FOLDER}" ; then
-    for apiFolderName in $( find "${API_DROPIN_FOLDER}/" 2>/dev/null | sort | uniq ) ; do
+    for apiFolderName in $( find "${API_DROPIN_FOLDER}/*" -type d 2>/dev/null | sort | uniq ) ; do
         apiFolder=$(basename ${apiFolderName})
-        echo "Calling API in: ${apiFolder}"
-        httpStatus=$(curl $(prop 'request-curl-commands') -d @${apiFolderName}/requestBody.json $(prop 'request-url') --insecure --write-out '%{http_code}' --silent --output /dev/null)
+        echo "API Folder: $apiFolder"
+        requestUrl=$(apiprops 'request-url')
+        curlParams=$(apiprops 'request-curl-commands')
+        echo "Calling API: $requestUrl with $curlParams"
+        httpStatus=$(curl $curlParams -d @${apiFolderName}/requestBody.json $requestUrl --insecure --write-out '%{http_code}' --silent --output /dev/null)
 
         echo "HTTP Status Code: ${httpStatus}"
     done
 fi
-
-
