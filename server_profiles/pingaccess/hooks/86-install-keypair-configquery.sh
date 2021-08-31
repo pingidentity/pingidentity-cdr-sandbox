552cef75309fa8fa37ec652c34f894bb7d07a199
ttranatping <ttran@pingidentity.com>
2021-08-31 20:02:49 +1000

    changing config listener certificate to generated

--- server_profiles/pingaccess/hooks/86-install-keypair-configquery.sh
+++ server_profiles/pingaccess/hooks/86-install-keypair-configquery.sh
@@ -3,9 +3,17 @@ generate_post_data()
 {
   cat <<EOF
 {
-  "alias": "pingaccess-configlistener-keypair",
-  "fileData": "$(cat network.p12 | base64)",
-  "password": "P@ssword1"
+  "alias": "config-query",
+  "city": "Melbourne",
+  "commonName": "pingaccess-admin",
+  "country": "AU",
+  "keyAlgorithm": "RSA",
+  "keySize": "2048",
+  "organization": "Ping Identity",
+  "organizationUnit": "APAC",
+  "signatureAlgorithm": "SHA256withRSA",
+  "state": "VIC",
+  "validDays": "365"
 }
 EOF
 }
@@ -22,20 +30,9 @@ generate_httpslistener_data()
 EOF
 }
 
-
-curl -o public.pem http://cdrregister:8084/public/download
-
-openssl genrsa -out client.key 2048
-openssl req -new -key client.key -out client.csr -subj "/CN=pingaccess-admin/O=My Company Name LTD./C=AU"
-
-curl -X POST -H --silent -F csr=@client.csr -o csrresponse.p7b http://cdrregister:8084/public/sign
-
-openssl pkcs7 -print_certs -in csrresponse.p7b -out csrresponse.pem
-openssl pkcs12 -export -out network.p12 -inkey client.key -in csrresponse.pem -password pass:P@ssword1
-
 echo $("generate_post_data") > install-cert.txt
 
-keypairId=$(curl -X POST --silent --basic -u Administrator:${PING_IDENTITY_PASSWORD} --header 'Content-Type: application/json' --header 'Accept: application/json' --header 'X-XSRF-Header: PingAccess' --data '@install-cert.txt' https://localhost:9000/pa-admin-api/v3/keyPairs/import --insecure | jq .id)
+keypairId=$(curl -X POST --silent --basic -u Administrator:${PING_IDENTITY_PASSWORD} --header 'Content-Type: application/json' --header 'Accept: application/json' --header 'X-XSRF-Header: PingAccess' --data '@install-cert.txt' https://localhost:9000/pa-admin-api/v3/keyPairs/generate --insecure | jq .id)
 
 if [ -z "$keypairId" ] || [ $keypairId == null ]
 then
@@ -49,5 +46,6 @@ else
   echo $("generate_httpslistener_data") > configure-https.txt
   configureHttpsResponse=$(curl -X PUT --silent --basic -u Administrator:${PING_IDENTITY_PASSWORD} --header 'Content-Type: application/json' --header 'Accept: application/json' --header 'X-XSRF-Header: PingAccess' --data '@configure-https.txt' https://localhost:9000/pa-admin-api/v3/httpsListeners/${httpsEngineId} --insecure)
 
-  echo "Keypair assigned to Admin HTTPS listener: $httpsEngineId"
+  echo "Keypair assigned to Config HTTPS listener: $httpsEngineId"
 fi
+
