07da5db8826feea9ef64fed6d6f34e6a4254e512
Tam Tran <ttran@pingidentity.com>
2023-03-16 08:54:08 +1100

    updating apk tools for pingaccess image

--- docker-compose/images/build-images.sh
+++ docker-compose/images/build-images.sh
@@ -1,1 +1,1 @@
-docker build --tag cdrsandbox/pingaccess --build-arg APKURL=http://dl-cdn.alpinelinux.org/alpine/v3.5/main/x86_64/apk-tools-static-2.6.10-r0.apk --file Dockerfile-pingaccess .
+docker build --tag cdrsandbox/pingaccess --build-arg APKURL=http://dl-cdn.alpinelinux.org/alpine/v3.9/main/x86_64/apk-tools-static-2.10.6-r0.apk --file Dockerfile-pingaccess .
